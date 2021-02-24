9f41904d2fed001e9130d6602c62e763d6ce6004
Anton Strogonoff <anton@strogonoff.name>
2021-02-24 09:08:57 +0000

    feat: update landing page & item class card

--- src/DefaultHomePage/index.tsx
+++ src/DefaultHomePage/index.tsx
@@ -4,13 +4,50 @@ import { useRouteData } from 'react-static';
 import { MainRegistryPageRouteData } from '../types';
 import Container from '../DefaultWidgets/Container';
 import { Locale } from '@riboseinc/paneron-registry-kit/types';
+import { IMetaBlock } from '../DefaultWidgets/MetaBlock';
+import { Classes, FormGroup, H5 } from '@blueprintjs/core';
+import ItemClassCard from '../DefaultWidgets/ItemClassCard';
 
 
 export default () => {
   const {
     register,
     statistics,
+    itemClassConfiguration,
+    subregisters,
   }: MainRegistryPageRouteData = useRouteData();
+
+  const metaBlocks: IMetaBlock[] = [{
+    title: "Content summary:",
+    content: <>
+      <p className={Classes.RUNNING_TEXT}>{register.contentSummary ?? 'N/A'}</p>
+    </>,
+  }, {
+    title: "Operating language:",
+    content: <>
+      <H5>
+        {register.operatingLanguage
+          ? <Locale locale={register.operatingLanguage} />
+          : 'N/A'}
+      </H5>
+      <FormGroup label="Alternative languages:">
+        {register.alternativeLanguages
+          ? register.alternativeLanguages.map((l, idx) => <Locale key={idx} locale={l} />)
+          : 'N/A'}
+      </FormGroup>
+    </>,
+  }, {
+    title: "Statistics:",
+    content: <>
+      <FormGroup label="Total number of items:">
+        <span style={{ fontSize: '140%' }}>
+          {statistics.totalItemCount !== undefined
+            ? statistics.totalItemCount
+            : 'N/A'}
+        </span>
+      </FormGroup>
+    </>,
+  }];
 
   return (
     <>
@@ -18,36 +55,15 @@ export default () => {
         <title>{register.name}</title>
       </Helmet>
 
-      <Container>
-        <dl>
-          <dt>Content summary</dt>
-          <dd>
-            {register.contentSummary ?? 'N/A'}
-          </dd>
-          <dt>Operating language</dt>
-          <dd>
-            {register.operatingLanguage
-              ? <Locale locale={register.operatingLanguage} />
-              : 'N/A'}
-          </dd>
-          <dt>Alternative languages</dt>
-          <dd>
-            {register.alternativeLanguages
-              ? register.alternativeLanguages.map((l, idx) => <Locale key={idx} locale={l} />)
-              : 'N/A'}
-          </dd>
-        </dl>
-
-        <h3>Statistics</h3>
-
-        <dl>
-          <dt>Total number of items</dt>
-          <dd>
-            {statistics.totalItemCount !== undefined
-              ? statistics.totalItemCount
-              : 'N/A'}
-          </dd>
-        </dl>
+      <Container metaBlocks={metaBlocks}>
+        {Object.entries(itemClassConfiguration).map(([classID, classData]) =>
+          <ItemClassCard
+            style={{ marginBottom: '1em' }}
+            itemClass={classData}
+            subregisters={subregisters}
+            key={classID}
+          />
+        )}
       </Container>
     </>
   );
@@ -57,7 +73,7 @@ export default () => {
 const Locale: React.FC<{ locale: Locale }> = function ({ locale }) {
   return (
     <>
-      {locale.name} • {locale.country} • {locale.languageCode}
+      {locale.name} ({locale.languageCode})
     </>
   );
 };
--- src/DefaultWidgets/ItemClassCard.tsx
+++ src/DefaultWidgets/ItemClassCard.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
-import { H5, Icon } from '@blueprintjs/core';
-import { ItemClassConfiguration } from '@riboseinc/paneron-registry-kit/types';
+import { ButtonGroup, H5, Icon } from '@blueprintjs/core';
+import { ItemClassConfiguration, Subregisters } from '@riboseinc/paneron-registry-kit/types';
 import { ButtonLink } from './linksButtons';
 import Card from './Card';
 
 
-export const ItemClassCard: React.FC<{ itemClass: ItemClassConfiguration<any> }> = function ({ itemClass }) {
+export const BaseItemClassCard: React.FC<{
+  itemClass: ItemClassConfiguration<any>
+  className?: string
+  style?: React.CSSProperties
+}> =
+function ({ itemClass, className, style, children }) {
   return (
-    <Card elevation={3}>
+    <Card elevation={3} className={className} style={style}>
       <H5><Icon icon="cube" />&ensp;{itemClass.meta.title}</H5>
       <p>{itemClass.meta.description}</p>
-      <ButtonLink relative to={`./${itemClass.meta.id}`}>View</ButtonLink>
+      {children}
     </Card>
   );
+};
+
+
+export const ItemClassCard: React.FC<{
+  itemClass: ItemClassConfiguration<any>
+  subregisters?: Subregisters
+  className?: string
+  style?: React.CSSProperties
+}> = function ({ itemClass, subregisters, className, style }) {
+  if (subregisters !== undefined) {
+    const containingSubregisters = Object.entries(subregisters).
+      filter(([_, subregData]) => subregData.itemClasses.indexOf(itemClass.meta.id) >= 0);
+
+    return (
+      <BaseItemClassCard itemClass={itemClass} className={className} style={style}>
+        <ButtonGroup style={{ overflowX: 'auto', width: '100%' }}>
+          {containingSubregisters.map(([subregID, subregData]) =>
+            <ButtonLink
+                key={subregID}
+                relative
+                to={`./${subregID}/${itemClass.meta.id}`}>
+              Browse&nbsp;{containingSubregisters.length > 1 ? subregData.title : "items"}
+            </ButtonLink>
+          )}
+        </ButtonGroup>
+      </BaseItemClassCard>
+    );
+
+  } else {
+    return (
+      <BaseItemClassCard itemClass={itemClass} className={className} style={style}>
+        <ButtonLink relative to={`./${itemClass.meta.id}`}>Browse items</ButtonLink>
+      </BaseItemClassCard>
+    );
+  }
 };
 
 
